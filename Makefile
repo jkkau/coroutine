3e175b952f049eb4ab95d6e1483a74cab016f480
jkkau <jkkau10@sina.com>
2025-04-27 11:01:38 +0800

    fix: 分配协程栈时使用16位字节对齐
    1.使用posix_memalign来分配协程栈空间
    2.astyle格式化代码格式

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all:
 	gcc main.c swap.s -g -o co
 
+format:
+	astyle --style=kr --indent=force-tab=4 ./*.c
+
 clean:
 	rm co
--- main.c
+++ main.c
@@ -4,52 +4,63 @@
 typedef void (*co_func)();
 
 typedef struct {
-    void *rsp;
-    void *rbp;
-    // void *eip;
-}co_context_t;
+	void *rsp;
+	void *rbp;
+	// void *eip;
+} co_context_t;
 
 #define STACK_SIZE 2 * 1024
 typedef struct {
-    co_context_t ctx;
-    void *stack;
-}coroutine_t;
+	co_context_t ctx;
+	void *stack;
+} coroutine_t;
 
 extern int swap(co_context_t *next_co, co_context_t *cur_co);
 
-void coroutine_init(coroutine_t *co, co_func f) {
-    co->stack = malloc(STACK_SIZE);
-    *(void **)(co->stack + STACK_SIZE - sizeof(void *)) = (void *)f;
-    co->ctx.rsp = co->stack + STACK_SIZE - sizeof(void *);
-    co->ctx.rbp = NULL;
+void coroutine_init(coroutine_t *co, co_func f)
+{
+	posix_memalign(&co->stack, 16, STACK_SIZE);
+	*(void **)(co->stack + STACK_SIZE - sizeof(void *)) = (void *)f;
+	co->ctx.rsp = co->stack + STACK_SIZE - sizeof(void *);
+	co->ctx.rbp = NULL;
+}
+
+void coroutine_destroy(coroutine_t *co)
+{
+	if (co->stack != NULL) {
+		free(co->stack);
+	}
 }
 
 coroutine_t coroutines[2];
 coroutine_t main_routine;
 
-void func0() {
-    printf("func0\n");
-    swap(&main_routine.ctx, &coroutines[0].ctx);
+void func0()
+{
+	printf("func0\n");
+	swap(&main_routine.ctx, &coroutines[0].ctx);
 
-    __builtin_unreachable();
+	__builtin_unreachable();
 }
 
-void func1() {
-    printf("func1\n");
-    swap(&main_routine.ctx, &coroutines[1].ctx);
+void func1()
+{
+	printf("func1\n");
+	swap(&main_routine.ctx, &coroutines[1].ctx);
 
-    __builtin_unreachable();
+	__builtin_unreachable();
 }
 
-int main() {
-    coroutine_init(&coroutines[0], func0);
-    coroutine_init(&coroutines[1], func1);
+int main()
+{
+	coroutine_init(&coroutines[0], func0);
+	coroutine_init(&coroutines[1], func1);
 
-    printf("start\n");
-    swap(&coroutines[0].ctx, &main_routine.ctx);
-    printf("fun0 finished.\n");
-    swap(&coroutines[1].ctx, &main_routine.ctx);
-    printf("func1 finished.\n");
+	printf("start\n");
+	swap(&coroutines[0].ctx, &main_routine.ctx);
+	printf("fun0 finished.\n");
+	swap(&coroutines[1].ctx, &main_routine.ctx);
+	printf("func1 finished.\n");
 
-    return 0;
+	return 0;
 }
